8c1e3733fb74ac3e38fc246034474fdfe24b7f9b
Shifas0897 <Shifaseid8.ss@gmail.com>
2024-11-26 21:42:31 +0300

    final commit

--- netflix_clone/vite.config.js
+++ netflix_clone/vite.config.js
@@ -4,24 +4,25 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Netflix_sev/",
+  
   plugins: [react()],
+  base: "/Netflix_sev/",
 
+});
 
-})
-;// vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+ /*vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//   plugins: [react()],
-//   root: './src', // Adjust if necessary
-//   base: '/Netflix_sev/', // Adjust if necessary
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: './src/main.jsx', // Adjust the path as needed
-//       },
-//     },
-//   },
-// });
+export default defineConfig({
+plugins: [react()],
+root: './src', // Adjust if necessary
+base: '/Netflix_sev/', // Adjust if necessary
+build: {
+rollupOptions: {
+input: {
+main: './src/main.jsx', // Adjust the path as needed
+      },
+    },
+   },
+ }) */
